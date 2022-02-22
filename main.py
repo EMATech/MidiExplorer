969132cece9c1349394196b1604eb617180822b5
Raphaël Doursenaud <rdoursenaud@gmail.com>
2022-02-22 11:55:48 +0100

    Add message type activity monitor
    Workaround fixed headers not working without upstream table scrolling support

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from dearpygui_ext.logger import mvLogger  # https://dearpygui-ext.readthedocs.i
 
 global logger, log_win, previous_timestamp, probe_data_counter
 
-DEBUG = True
+DEBUG = False
 INIT_FILENAME = "midiexplorer.ini"
 START_TIME = time.time()
 
@@ -308,10 +308,13 @@ def _add_probe_data(timestamp: float, source: str, data: mido.Message) -> None:
             _mon_blink('s')
         # Status
         dpg.add_text(data.type)
+        _mon_blink(data.type)
         # Data 1 & 2
         if 'note' in data.type:
             dpg.add_text(data.note)  # TODO: decode to human readable
             dpg.add_text(data.velocity)
+            if data.velocity == 0:  # This is equivalent to a note_off message
+                _mon_blink('note_off')
         elif 'control' in data.type:
             dpg.add_text(data.control)  # TODO: decode to human readable
             dpg.add_text(data.value)
@@ -325,6 +328,9 @@ def _add_probe_data(timestamp: float, source: str, data: mido.Message) -> None:
             # TODO: decode other types
             dpg.add_text("")
             dpg.add_text("")
+
+    # TODO: per message type color
+    # dpg.highlight_table_row(table_id, i, [255, 0, 0, 100])
 
     # Autoscroll
     if dpg.get_value('probe_data_table_autoscroll'):
@@ -348,8 +354,44 @@ def _update_blink_status():
         now = time.time() - START_TIME
         if dpg.get_value(f"mon_{channel}_active_until") < now:
             dpg.bind_item_theme(f"mon_{channel}", None)
-    if dpg.get_value(f"mon_s_active_until") < now:
-        dpg.bind_item_theme(f"mon_s", None)
+    if dpg.get_value('mon_s_active_until') < now:
+        dpg.bind_item_theme('mon_s', None)
+    if dpg.get_value('mon_active_sensing_active_until') < now:
+        dpg.bind_item_theme('mon_active_sensing', None)
+    if dpg.get_value('mon_note_off_active_until') < now:
+        dpg.bind_item_theme('mon_note_off', None)
+    if dpg.get_value('mon_note_on_active_until') < now:
+        dpg.bind_item_theme('mon_note_on', None)
+    if dpg.get_value('mon_polytouch_active_until') < now:
+        dpg.bind_item_theme('mon_polytouch', None)
+    if dpg.get_value('mon_control_change_active_until') < now:
+        dpg.bind_item_theme('mon_control_change', None)
+    if dpg.get_value('mon_program_change_active_until') < now:
+        dpg.bind_item_theme('mon_program_change', None)
+    if dpg.get_value('mon_aftertouch_active_until') < now:
+        dpg.bind_item_theme('mon_aftertouch', None)
+    if dpg.get_value('mon_pitchwheel_active_until') < now:
+        dpg.bind_item_theme('mon_pitchwheel', None)
+    if dpg.get_value('mon_sysex_active_until') < now:
+        dpg.bind_item_theme('mon_sysex', None)
+    if dpg.get_value('mon_quarter_frame_active_until') < now:
+        dpg.bind_item_theme('mon_quarter_frame', None)
+    if dpg.get_value('mon_songpos_active_until') < now:
+        dpg.bind_item_theme('mon_songpos', None)
+    if dpg.get_value('mon_song_select_active_until') < now:
+        dpg.bind_item_theme('mon_song_select', None)
+    if dpg.get_value('mon_tune_request_active_until') < now:
+        dpg.bind_item_theme('mon_tune_request', None)
+    if dpg.get_value('mon_clock_active_until') < now:
+        dpg.bind_item_theme('mon_clock', None)
+    if dpg.get_value('mon_start_active_until') < now:
+        dpg.bind_item_theme('mon_start', None)
+    if dpg.get_value('mon_continue_active_until') < now:
+        dpg.bind_item_theme('mon_continue', None)
+    if dpg.get_value('mon_stop_active_until') < now:
+        dpg.bind_item_theme('mon_stop', None)
+    if dpg.get_value("mon_reset_active_until") < now:
+        dpg.bind_item_theme('mon_reset', None)
 
 
 def _clear_probe_data_table():
@@ -372,11 +414,29 @@ if __name__ == '__main__':
         dpg.configure_app(init_file=INIT_FILENAME)
 
     with dpg.value_registry():
-        dpg.add_string_value(tag="generator_decoded_message", default_value="")
+        dpg.add_string_value(tag='generator_decoded_message', default_value='')
         dpg.add_float_value(tag='blink_duration', default_value=.25)  # seconds
         for channel in range(16):  # Monitoring status
             dpg.add_float_value(tag=f"mon_{channel}_active_until", default_value=0)  # seconds
-        dpg.add_float_value(tag=f"mon_s_active_until", default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_s_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_active_sensing_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_note_off_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_note_on_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_polytouch_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_control_change_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_program_change_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_aftertouch_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_pitchwheel_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_sysex_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_quarter_frame_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_songpos_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_song_select_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_tune_request_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_clock_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_start_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_continue_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_stop_active_until', default_value=0)  # seconds
+        dpg.add_float_value(tag='mon_reset_active_until', default_value=0)  # seconds
 
     with dpg.window(
             tag='main_win',
@@ -499,53 +559,134 @@ if __name__ == '__main__':
         # TODO: timegraph?
 
         # Input Activity Monitor
-        dpg.add_child_window(tag='act_mon', label="Input activity monitor", height=40)
+        dpg.add_child_window(tag='act_mon', label="Input activity monitor", height=65, border=False)
 
         with dpg.table(parent='act_mon', header_row=False, policy=dpg.mvTable_SizingFixedFit):
+            dpg.add_table_column(width_stretch=True)  # Blink duration slider
             for channel in range(17):
                 dpg.add_table_column()
-            dpg.add_table_column(width_stretch=True)  # Blink duration slider
 
             with dpg.theme(tag='__red'):
                 with dpg.theme_component(dpg.mvButton):
                     dpg.add_theme_color(dpg.mvThemeCol_Button, (255, 0, 0))
 
             with dpg.table_row():
-                for channel in range(16):  # Channel messages
-                    dpg.add_button(tag=f"mon_{channel}", label=f"{channel + 1}")
-                dpg.add_button(tag='mon_s', label="S")  # System message
-                dpg.add_slider_float(tag='blink_duration_slider', label="Persistence (s)",
+                dpg.add_slider_float(tag='blink_duration_slider',
                                      min_value=0, max_value=0.5, source='blink_duration',
                                      callback=lambda:
                                      dpg.set_value('blink_duration', dpg.get_value('blink_duration_slider')))
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Persistence (s)")
+                for channel in range(16):  # Channel messages
+                    dpg.add_button(tag=f"mon_{channel}", label=f"{channel + 1}")
+                    with dpg.tooltip(dpg.last_item()):
+                        dpg.add_text(f"Channel {channel + 1}")
+                dpg.add_button(tag='mon_s', label="S")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("System Message")
+            with dpg.table_row():
+                dpg.add_button(tag='mon_active_sensing', label=" AS ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Active Sensing")
+                dpg.add_button(tag='mon_note_off', label="OFF ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Note Off")
+                dpg.add_button(tag='mon_note_on', label=" ON ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Note On")
+                dpg.add_button(tag='mon_polytouch', label="POLY")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Polytouch (aka Pressure)")
+                dpg.add_button(tag='mon_control_change', label=" CC ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Control Change")
+                dpg.add_button(tag='mon_program_change', label=" PC ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Program Change")
+                dpg.add_button(tag='mon_aftertouch', label="AFTT")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("After Touch (aka Channel Pressure)")
+                dpg.add_button(tag='mon_pitchwheel', label="PTCH")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Pitch Wheel")
+                dpg.add_button(tag='mon_sysex', label="SYX ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("System Exclusive (aka SysEx)")
+                dpg.add_button(tag='mon_quarter_frame', label=" QF ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("MTC SMPTE Quarter Frame")
+                dpg.add_button(tag='mon_songpos', label="SGPS")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Song Position")
+                dpg.add_button(tag='mon_song_select', label="SGSL")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Song Select")
+                dpg.add_button(tag='mon_tune_request', label=" TR ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Tune Request")
+                dpg.add_button(tag='mon_clock', label="CLK ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Clock")
+                dpg.add_button(tag='mon_start', label="STRT")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Start")
+                dpg.add_button(tag='mon_continue', label="CTNU")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Continue")
+                dpg.add_button(tag='mon_stop', label="STOP")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Stop")
+                dpg.add_button(tag='mon_reset', label="RST ")
+                with dpg.tooltip(dpg.last_item()):
+                    dpg.add_text("Reset")
+
+        dpg.add_child_window(tag='probe_table_container')
 
         # Details buttons
         # FIXME: separated to not scroll with table child window until table scrolling is supported
-        dpg.add_child_window(tag='act_det_btns', label="Buttons", height=40)
+        dpg.add_child_window(parent='probe_table_container', tag='act_det_btns', label="Buttons", height=40,
+                             border=False)
         with dpg.group(parent='act_det_btns', horizontal=True):
             dpg.add_checkbox(tag='probe_data_table_autoscroll', label="Auto-Scroll", default_value=True)
             dpg.add_button(label="Clear", callback=_clear_probe_data_table)
 
         # Details
-        dpg.add_child_window(tag='act_det', label="Details", height=570)
-
-        with dpg.table(parent='act_det',
-                       tag='probe_data_table',
+        # FIXME: workaround table scrolling not implemented upstream yet to have static headers
+        # dpg.add_child_window(tag='act_det_headers', label="Details headers", height=5, border=False)
+        with dpg.table(parent='act_det_btns',
+                       tag='probe_data_table_headers',
                        header_row=True,
                        freeze_rows=1,
-                       policy=dpg.mvTable_SizingFixedFit,
-                       # scrollY=True,  # FIXME: Scroll the table instead of the window when available upstream
-                       clipper=False):
+                       policy=dpg.mvTable_SizingStretchSame):
             dpg.add_table_column(label="Timestamp (ms)")
             dpg.add_table_column(label="Delta (ms)")
             dpg.add_table_column(label="Source")
             dpg.add_table_column(label="Raw Message (HEX)")
             if DEBUG:
-                dpg.add_table_column(label="Decoded Message", width_stretch=True)
+                dpg.add_table_column(label="Decoded Message")
             dpg.add_table_column(label="Channel")
-            dpg.add_table_column(label="Status", width_stretch=True)
-            dpg.add_table_column(label="Data 1", width_stretch=True)
-            dpg.add_table_column(label="Data 2", width_stretch=True)
+            dpg.add_table_column(label="Status")
+            dpg.add_table_column(label="Data 1")
+            dpg.add_table_column(label="Data 2")
+
+        dpg.add_child_window(parent='probe_table_container', tag='act_det', label="Details", height=530, border=False)
+        with dpg.table(parent='act_det',
+                       tag='probe_data_table',
+                       header_row=False,  # FIXME: True when table scrolling will be implemented upstream
+                       freeze_rows=0,  # FIXME: 1 when table scrolling will be implemented upstream
+                       policy=dpg.mvTable_SizingStretchSame,
+                       # scrollY=True,  # FIXME: Scroll the table instead of the window when available upstream
+                       clipper=True):
+            dpg.add_table_column(label="Timestamp (ms)")
+            dpg.add_table_column(label="Delta (ms)")
+            dpg.add_table_column(label="Source")
+            dpg.add_table_column(label="Raw Message (HEX)")
+            if DEBUG:
+                dpg.add_table_column(label="Decoded Message")
+            dpg.add_table_column(label="Channel")
+            dpg.add_table_column(label="Status")
+            dpg.add_table_column(label="Data 1")
+            dpg.add_table_column(label="Data 2")
 
             _init_details_table_data()
 
